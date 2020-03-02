689448efceefc9c6396211aebdc074183ebd2ca4
marcush12 <marcos012santos@gmail.com>
2020-03-02 09:27:14 -0300

    continuous deploy netlify

---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'gatsby'
+import Layout from '../components/Layout'
+
+const blog = () => {
+  return (
+    <Layout>
+      Hi from blog page!!
+      <Link to='/'>back home!</Link>
+    </Layout>
+  )
+}
+
+export default blog
+
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from '../components/Layout'
+import SimpleHero from '../components/SimpleHero'
+import Banner from '../components/Banner'
+import About from '../components/Home/About'
+import Services from '../components/Home/Services'
+
+export default () => (
+    <Layout>
+      <SimpleHero>
+        <Banner title='continue exploring' info='Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, natus?'>
+          <Link className='btn-white' to='/tours'>explore tours</Link>
+        </Banner>    
+      </SimpleHero>
+      <About />
+      <Services />
+    </Layout>
+)
